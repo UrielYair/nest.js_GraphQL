c33ef2b559ddd400425c23acc72c8d2a8bb719ba
Uriel Yair <uriel91@gmail.com>
2020-04-21 22:33:59 +0300

    base files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { GraphQLModule } from '@nestjs/graphql'
+import { CompanyModule } from './company/company.module';
+import { Company } from './company/company.entity';
+import { EmployeeModule } from './employee/employee.module';
+import { Employee } from './employee/employee.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/a_holdings',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [
+        Company,
+        Employee
+      ]
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true
+    }),
+    CompanyModule,
+    EmployeeModule
+  ]
 })
 export class AppModule {}
